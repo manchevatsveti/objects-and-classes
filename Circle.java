f04a33af32a9aeb09578633819e36b7cc4d42ae1
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-22 16:21:09 +0300

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,19 @@
 package eddu.smg;
-
+import java. lang. Math;
 public class Circle {
-
+public double radius =1;
+Circle(){
+}
+Circle(double NewRadius){
+	radius = NewRadius;
+}
+public double GetArea(){
+	return radius*radius*Math.PI;
+}
+public double getP(){
+	return 2*radius*Math.PI;
+}
+public void setNewR(double NewRadius){
+	radius = NewRadius;
+}
 }
