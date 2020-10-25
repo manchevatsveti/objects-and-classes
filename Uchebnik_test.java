af086f5ccbf46d0ece0e2d9d495996f4f5ea7475
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-25 21:21:24 +0200

    Update Uchebnik_test.java

--- Uchebnik_test.java
+++ Uchebnik_test.java
@@ -7,7 +7,8 @@ public class Uchebnik_test {
 		Scanner in = new Scanner(System.in);
 	uchebnik[] students = new uchebnik[5];
  
-		for(int i=0;i<5;i++) {
+		for(int i=0;i<students.length;i++) {
+			students[i]= new uchebnik();
 			System.out.println("Name of student" + (i+1)+ " ?");
 			students[i].getName()= in.next();
 			System.out.println("Average of student" + (i+1)+ " ?");
@@ -17,7 +18,7 @@ public class Uchebnik_test {
 	}
 
 	public void uchenitsi (uchebnik[] st) {
-		for(int i=0;i<5;i++) {
+		for(int i=0;i<st.length;i++) {
 		if(st[i].stipendia(st[i])) {
 			System.out.println(st[i].getName());
 		}
