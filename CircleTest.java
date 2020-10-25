a73c17e939e77a7b2cb1dc47ef581d82d3eaf23a
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-25 20:51:55 +0200

    Update CircleTest.java

--- CircleTest.java
+++ CircleTest.java
@@ -1,19 +1,18 @@
 package eddu.smg;
 
-public class CircleTest {
-
+public class Circle_test {
 	public static void main(String[] args) {
 		Circle circle1 =new Circle();
-		System.out.println("Radius: "+ circle1.radius + "Area: "+ circle1.GetArea() );
-
+		
+		System.out.println("Radius: "+ circle1.getRadius() + " Area: "+ circle1.GetArea() );
+		circle1.setRadius(3);
+		System.out.println("Radius: "+ circle1.getRadius() + " Area: "+ circle1.GetArea() );
 		Circle circle2 = new Circle(25);
-		System.out.println("Radius: "+ circle2.radius + "Area: "+ circle2.GetArea() );
+		System.out.println("Radius: "+ circle2.getRadius() + " Area: "+ circle2.GetArea() );
 		
 		Circle circle3 =new Circle(125);
-		System.out.println("Radius: "+ circle3.radius + "Area: "+ circle3.GetArea() );
+		System.out.println("Radius: "+ circle3.getRadius() + " Area: "+ circle3.GetArea() );
 	
 		circle2.setNewR(100);
-		System.out.println("Radius: "+ circle2.radius + "Area: "+ circle2.GetArea()+ "Perimeter: " + circle2.getP() );
+		System.out.println("Radius: "+ circle2.getRadius() + " Area: "+ circle2.GetArea()+ "Perimeter: " + circle2.getP() );
 	}
-
-}
