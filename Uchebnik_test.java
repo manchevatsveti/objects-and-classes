4ad8f4ffa0cb8ca1636863819ccfa27e7d2f7f88
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-26 15:06:06 +0200

    Update Uchebnik_test.java

--- Uchebnik_test.java
+++ Uchebnik_test.java
@@ -1,25 +1,29 @@
 package eddu.smg;
 import java.util.Scanner;
 
-public class Uchebnik_test {
+public class uchebnik_test {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-	uchebnik[] students = new uchebnik[5];
+		uchenik[] students = new uchenik[5];
+		String name;
+		double average;
  
 		for(int i=0;i<students.length;i++) {
-			students[i]= new uchebnik();
+			
 			System.out.println("Name of student" + (i+1)+ " ?");
-			students[i].getName()= in.next();
+			name = in.next();
 			System.out.println("Average of student" + (i+1)+ " ?");
-			students[i].getAverage()= in.nextDouble();
+			average = in.nextDouble();
+			students[i]= new uchenik(name, average);
 		}
 		
+		uchenitsi(students);
 	}
 
-	public void uchenitsi (uchebnik[] st) {
+	public static void uchenitsi (uchenik[] st) {
 		for(int i=0;i<st.length;i++) {
-		if(st[i].stipendia(st[i])) {
+		if(st[i].stipendia(st[i].getAverage())) {
 			System.out.println(st[i].getName());
 		}
 	}
