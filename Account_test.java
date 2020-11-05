1b2380a12cbdb87d993ee617c650f74d2fd7e3df
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-11-05 22:38:53 +0200

    Update Account_test.java

--- Account_test.java
+++ Account_test.java
@@ -5,7 +5,7 @@ public class Account_test {
 	public static void main(String[] args) {
 		Account a1 = new Account(1122,20000);
 		Account.setYearInterestRate(4.5);
-		a1.withdraw(2000);
+		a1.withdraw(2500);
 		a1.deposit(3000);
 		System.out.println("Balance: "+ a1.getBalance()+ " Monthly Interest: "+ a1.getMonthlyInterest()+ " Date: "+ a1.getDateCreated());
 	}
